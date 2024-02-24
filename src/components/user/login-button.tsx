cabd8e5cbe147b09429ff60f3289486ff63619b2
harsha <ttrgharsha2001@gmail.com>
2024-02-24 15:15:13 +0530

    Decouple buttons from core

--- src/components/user/login-button.tsx
+++ src/components/user/login-button.tsx
@@ -1,13 +1,12 @@
 import { FC } from "react";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useAppContext } from "../../middleware/context-provider";
 import Button from "@mui/material/Button";
 
 export const LogInButton: FC = () => {
-  const auth = getAuth();
-  const provider = new GoogleAuthProvider();
+    const dispatch = useAppContext()[1];
 
   const onLoginClick = () => {
-    signInWithPopup(auth, provider);
+    dispatch({ type: "LOGIN" });
   };
 
   return (
