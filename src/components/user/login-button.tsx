8f91382c92e4c094e4f1cec324e49bea5419be7c
harsha <ttrgharsha2001@gmail.com>
2024-02-24 15:07:13 +0530

    Add material UI

--- src/components/user/login-button.tsx
+++ src/components/user/login-button.tsx
@@ -1,5 +1,6 @@
 import { FC } from "react";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import Button from "@mui/material/Button";
 
 export const LogInButton: FC = () => {
   const auth = getAuth();
@@ -9,5 +10,11 @@ export const LogInButton: FC = () => {
     signInWithPopup(auth, provider);
   };
 
-  return <button onClick={onLoginClick}>Log in</button>;
+  return (
+    <>
+      <Button onClick={onLoginClick} variant="contained">
+        Log in
+      </Button>
+    </>
+  );
 };
