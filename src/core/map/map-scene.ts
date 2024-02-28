91465147580ea02929556e950c699d215ba01204
harsha <ttrgharsha2001@gmail.com>
2024-02-28 10:58:38 +0530

    setup mapbox scene

--- src/core/map/map-scene.ts
+++ src/core/map/map-scene.ts
@@ -1,14 +1,17 @@
 import * as THREE from "three";
-import mapboxgl from "mapbox-gl";
+import * as OBC from "openbim-components";
+import * as MAPBOX from "mapbox-gl";
 import { CSS2DObject } from "three/examples/jsm/renderers/CSS2DRenderer";
-import { GisParameters, Building, LngLat } from "./types";
+import { GisParameters, Building, LngLat } from "../../types";
 import { MAPBOX_KEY } from "../../config";
 import { User } from "firebase/auth";
 import { MapDatabase } from "./map-database";
 import { Events } from "../../middleware/event-handler";
 
 export class MapScene {
-  private map: mapboxgl.Map;
+  private components = new OBC.Components();
+  private readonly style = "mapbox://styles/mapbox/light-v10";
+  private map: MAPBOX.Map;
   private clickedCoordinates: LngLat = { lat: 0, lng: 0 };
   private center: LngLat = { lat: 0, lng: 0 };
   private labels: { [id: string]: CSS2DObject } = {};
@@ -19,13 +22,17 @@ export class MapScene {
     this.events = events;
     const config = this.getConfig(container);
     this.map = this.createMap(config);
+    this.initializeComponent(config);
     this.createScene();
   }
 
   dispose() {
-    this.map.remove();
+    this.components.dispose();
+    (this.map as any) = null;
+    (this.components as any) = null;
     for (const id in this.labels) {
       const label = this.labels[id];
+      label.removeFromParent();
       label.element.remove();
     }
     this.labels = {};
@@ -33,13 +40,14 @@ export class MapScene {
 
   async getAllBuildings(user: User) {
     const buildings = await this.database.getBuildings(user);
+    if (!this.components) return;
     this.addToScene(buildings);
   }
 
   async addBuilding(user: User) {
     const { lat, lng } = this.clickedCoordinates;
     const userID = user.uid;
-    const building = { userID, lat, lng, uid: "" };
+    const building = { userID, lat, lng, uid: "", name: "", models: [] };
     building.uid = await this.database.add(building);
     this.addToScene([building]);
   }
@@ -52,19 +60,22 @@ export class MapScene {
 
       const label = new CSS2DObject(htmlElement);
 
-      const coords = mapboxgl.MercatorCoordinate.fromLngLat([lng, lat]);
+      const center = MAPBOX.MercatorCoordinate.fromLngLat(
+        { ...this.center },
+        0
+      );
 
-      const centerCoords = mapboxgl.MercatorCoordinate.fromLngLat([
-        this.center.lng,
-        this.center.lat,
-      ]);
+      const units = center.meterInMercatorCoordinateUnits();
 
-      const modelX = coords.x - centerCoords.x;
-      const modelY = coords.y - centerCoords.y;
+      const model = MAPBOX.MercatorCoordinate.fromLngLat({ lng, lat }, 0);
+      model.x /= units;
+      model.y /= units;
+      center.x /= units;
+      center.y /= units;
 
-      label.position.set(modelX, 0, modelY);
+      label.position.set(model.x - center.x, 0, model.y - center.y);
 
-      this.map.getContainer().appendChild(label.element);
+      this.components.scene.get().add(label);
       this.labels[uid] = label;
     }
   }
@@ -73,33 +84,45 @@ export class MapScene {
     const div = document.createElement("div");
     div.textContent = "🏢";
     div.onclick = () => {
-        this.events.trigger({ type: "OPEN_BUILDING", payload: id });
-      };
+      this.events.trigger({ type: "OPEN_BUILDING", payload: id });
+    };
     div.classList.add("thumbnail");
     return div;
   }
 
-  private createMap(config: GisParameters & { accessToken: string }) {
-    const map = new mapboxgl.Map({
-      container: config.container,
-      style: "mapbox://styles/mapbox/light-v10",
-      center: [config.center[0], config.center[1]],
-      zoom: config.zoom,
-      pitch: config.pitch,
-      bearing: config.bearing,
+  private initializeComponent(config: GisParameters) {
+    this.components.scene = new OBC.SimpleScene(this.components);
+    this.components.camera = new OBC.MapboxCamera();
+    this.components.renderer = this.createRenderer(config);
+    this.components.init();
+  }
+
+  private getCoordinates(config: GisParameters) {
+    const merc = MAPBOX.MercatorCoordinate;
+    return merc.fromLngLat(config.center, 0);
+  }
+
+  private createRenderer(config: GisParameters) {
+    const coords = this.getCoordinates(config);
+    return new OBC.MapboxRenderer(this.components, this.map, coords);
+  }
+
+  private createMap(config: GisParameters) {
+    const map = new MAPBOX.Map({
+      ...config,
+      style: this.style,
       antialias: true,
-      accessToken: config.accessToken,
     });
     map.on("contextmenu", this.storeMousePosition);
     return map;
   }
 
-  private storeMousePosition = (event: mapboxgl.MapMouseEvent) => {
-    this.clickedCoordinates = event.lngLat;
+  private storeMousePosition = (event: MAPBOX.MapMouseEvent) => {
+    this.clickedCoordinates = { ...event.lngLat };
   };
 
   private createScene() {
-    const scene = new THREE.Scene();
+    const scene = this.components.scene.get();
     scene.background = null;
     const directionalLight = new THREE.DirectionalLight(0xffffff);
     directionalLight.position.set(0, -70, 100).normalize();
@@ -110,17 +133,16 @@ export class MapScene {
   }
 
   private getConfig(container: HTMLDivElement) {
-    const center: [number, number] = [7.730277288470006, 63.110047455818375];
-    this.center = { lng: center[0], lat: center[1] }; // Optionally, you can assign this.center for consistency
+    const center = [76.95884245733829,11.022957822087053] as [number, number];
+    this.center = { lng: center[0], lat: center[1] };
     return {
       container,
-      zoom: 20.35,
+      accessToken: MAPBOX_KEY,
+      zoom:20.35,
       pitch: 60,
       bearing: -40,
-      center, // Modify center to be an array of numbers
+      center,
       buildings: [],
-      accessToken: MAPBOX_KEY, // Include accessToken here
     };
   }
-  
 }
